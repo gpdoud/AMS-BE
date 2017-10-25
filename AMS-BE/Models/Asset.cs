106a7781c2a9ce983168f5222e82392c2169b641
Greg doud <gpdoud@gmail.com>
2017-10-25 16:38:02 -0400

    Added Asset and ServiceHistory, but not done with either

---
+++ AMS-BE/Models/Asset.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AMS_BE.Models {
+	public class Asset {
+		public int Id { get; set; }
+		public DateTime? DatePurchased { get; set; }
+		[StringLength(30)]
+		[Required]
+		public string PO { get; set; }
+		public double? Cost { get; set; }
+		[StringLength(30)]
+		[Required]
+		public string BudgetCenter { get; set; }
+		[ForeignKey("User")]
+		public User AssignedToUser { get; set; }
+		[StringLength(30)]
+		[Required]
+		public string DepartmentName { get; set; }
+		public DateTime? DateOutForRepair { get; set; }
+		public DateTime? DateReturnedToService { get; set; }
+		[StringLength(30)]
+		public string ReasonForRepair { get; set; }
+		public string HistoryOfChange { get; set; } // don't know what this is
+		public DateTime? DateRetired { get; set; }
+		public DateTime? DateSurplused { get; set; }
+		public DateTime? DateDisposed { get; set; }
+		public double? ResidualValue { get; set; }
+	}
+}
---
+++ AMS-BE/Models/ServiceHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMS_BE.Models {
+	public class ServiceHistory {
+		public int Id { get; set; }
+
+		public bool Active { get; set; }
+		public DateTime DateCreated { get; set; }
+		public DateTime? DateUpdated { get; set; }
+	}
+}
